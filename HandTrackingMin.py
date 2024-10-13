6fb7574210a3aa5bf422938b942f048f20f90d06
John Montgomery <j.ace.svg@gmail.com>
2024-10-13 14:23:03 -0400

    Calculate ball bouncing off of paddles and update all var/func naming to snaketail

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -8,27 +8,27 @@ from functools import partial
 
 cap = cv2.VideoCapture(0 + cv2.CAP_DSHOW)
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands(static_image_mode=False,
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(static_image_mode=False,
                       model_complexity=1,
                       min_detection_confidence=0.75,
                       min_tracking_confidence=0.75,
                       max_num_hands=2)
-mpDraw = mp.solutions.drawing_utils
+mp_draw = mp.solutions.drawing_utils
 
 previous_time = 0
 current_time = 0
 
-def calc_landmark_distance(fingerCoordinates, landmarkStart, landmarkEnd):
+def calc_landmark_distance(finger_coordinates, landmark_start, landmark_end):
     return math.sqrt(
-                      math.pow(fingerCoordinates[landmarkStart][0] - fingerCoordinates[landmarkEnd][0], 2) +
-                      math.pow(fingerCoordinates[landmarkStart][1] - fingerCoordinates[landmarkEnd][1], 2) +
-                      math.pow(fingerCoordinates[landmarkStart][2] - fingerCoordinates[landmarkEnd][2], 2)
-                    ) / fingerCoordinates["scalar"]
+                      pow(finger_coordinates[landmark_start][0] - finger_coordinates[landmark_end][0], 2) +
+                      pow(finger_coordinates[landmark_start][1] - finger_coordinates[landmark_end][1], 2) +
+                      pow(finger_coordinates[landmark_start][2] - finger_coordinates[landmark_end][2], 2)
+                    ) / finger_coordinates["scalar"]
 
 def process_hands(results):
-    fingerCoordinates = {}
-    mpDraw.draw_landmarks(img, handLandmarks, mpHands.HAND_CONNECTIONS)
+    finger_coordinates = {}
+    mp_draw.draw_landmarks(img, handLandmarks, mp_hands.HAND_CONNECTIONS)
 
     for id, landmark in enumerate(handLandmarks.landmark):
         #print(id, lm)
@@ -36,27 +36,27 @@ def process_hands(results):
         centerx, centery, centerz = int(landmark.x*width), int(landmark.y*height), int(landmark.z*height)
         match id:
             case 0: # Wrist
-                fingerCoordinates["wrist"] = (centerx, centery, centerz)
+                finger_coordinates["wrist"] = (centerx, centery, centerz)
             case 2: # Thumb knuckle
-                fingerCoordinates["thumb_k"] = (centerx, centery, centerz)
+                finger_coordinates["thumb_k"] = (centerx, centery, centerz)
             case 5: # Index tip
-                fingerCoordinates["index_k"] = (centerx, centery, centerz)
+                finger_coordinates["index_k"] = (centerx, centery, centerz)
             case 9: # Middle knuckle
-                fingerCoordinates["middle_k"] = (centerx, centery, centerz)
+                finger_coordinates["middle_k"] = (centerx, centery, centerz)
             case 13: # Ring knuckle
-                fingerCoordinates["ring_k"] = (centerx, centery, centerz)
+                finger_coordinates["ring_k"] = (centerx, centery, centerz)
             case 17: # Pinky knuckle
-                fingerCoordinates["pinky_k"] = (centerx, centery, centerz)
+                finger_coordinates["pinky_k"] = (centerx, centery, centerz)
             case 4: # Thumb tip
-                fingerCoordinates["thumb_t"] = (centerx, centery, centerz)
+                finger_coordinates["thumb_t"] = (centerx, centery, centerz)
             case 8: # Index tip
-                fingerCoordinates["index_t"] = (centerx, centery, centerz)
+                finger_coordinates["index_t"] = (centerx, centery, centerz)
             case 12: # Middle tip
-                fingerCoordinates["middle_t"] = (centerx, centery, centerz)
+                finger_coordinates["middle_t"] = (centerx, centery, centerz)
             case 16: # Ring tip
-                fingerCoordinates["ring_t"] = (centerx, centery, centerz)
+                finger_coordinates["ring_t"] = (centerx, centery, centerz)
             case 20: # Pinky tip
-                fingerCoordinates["pinky_t"] = (centerx, centery, centerz)
+                finger_coordinates["pinky_t"] = (centerx, centery, centerz)
             case _:
                 pass
         if id in (5, 17, 0):
@@ -64,10 +64,10 @@ def process_hands(results):
         cv2.putText(img,str(id), (centerx + 10, centery + 10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
     
     # Cache distance scaling factor
-    fingerCoordinates["scalar"] = math.sqrt(
-        math.pow(fingerCoordinates["index_k"][0] - fingerCoordinates["wrist"][0], 2) + 
-        math.pow(fingerCoordinates["index_k"][1] - fingerCoordinates["wrist"][1], 2) + 
-        math.pow(fingerCoordinates["index_k"][2] - fingerCoordinates["wrist"][2], 2)
+    finger_coordinates["scalar"] = math.sqrt(
+        pow(finger_coordinates["index_k"][0] - finger_coordinates["wrist"][0], 2) + 
+        pow(finger_coordinates["index_k"][1] - finger_coordinates["wrist"][1], 2) + 
+        pow(finger_coordinates["index_k"][2] - finger_coordinates["wrist"][2], 2)
     )
 
     gesture_distances = {
@@ -89,105 +89,105 @@ def process_hands(results):
     extend_list = [False, False, False, False, False]
 
     # Pinch index Gesture
-    if fingerCoordinates.keys() >= {"thumb_t", "index_t"}:
-        if (calc_landmark_distance(fingerCoordinates, "thumb_t", "index_t") < gesture_distances["pinch-index"]):
+    if finger_coordinates.keys() >= {"thumb_t", "index_t"}:
+        if (calc_landmark_distance(finger_coordinates, "thumb_t", "index_t") < gesture_distances["pinch-index"]):
             #print("pinch index")
             pinch_list[0] = True
             pass
 
     # Pinch middle Gesture
-    if fingerCoordinates.keys() >= {"thumb_t", "middle_t"}:
-        if (calc_landmark_distance(fingerCoordinates, "thumb_t", "middle_t") < gesture_distances["pinch-middle"]):
+    if finger_coordinates.keys() >= {"thumb_t", "middle_t"}:
+        if (calc_landmark_distance(finger_coordinates, "thumb_t", "middle_t") < gesture_distances["pinch-middle"]):
             #print("pinch middle")
             pinch_list[1] = True
 
     # Pinch ring Gesture
-    if fingerCoordinates.keys() >= {"thumb_t", "ring_t"}:
-        if (calc_landmark_distance(fingerCoordinates, "thumb_t", "ring_t") < gesture_distances["pinch-ring"]):
+    if finger_coordinates.keys() >= {"thumb_t", "ring_t"}:
+        if (calc_landmark_distance(finger_coordinates, "thumb_t", "ring_t") < gesture_distances["pinch-ring"]):
             #print("pinch ring")
             pinch_list[2] = True
 
     # Pinch pinky Gesture
-    if fingerCoordinates.keys() >= {"thumb_t", "pinky_t"}:
-        if (calc_landmark_distance(fingerCoordinates, "thumb_t", "pinky_t") < gesture_distances["pinch-pinky"]):
+    if finger_coordinates.keys() >= {"thumb_t", "pinky_t"}:
+        if (calc_landmark_distance(finger_coordinates, "thumb_t", "pinky_t") < gesture_distances["pinch-pinky"]):
             #print("pinch pinky")
             pinch_list[3] = True
 
     # Extend thumb Gesture
-    if fingerCoordinates.keys() >= {"thumb_t", "thumb_k"}:
-        if (calc_landmark_distance(fingerCoordinates, "thumb_t", "index_k") > gesture_distances["split_thumb-index"] and
-            calc_landmark_distance(fingerCoordinates, "thumb_t", "wrist") > calc_landmark_distance(fingerCoordinates, "thumb_k", "wrist")):
+    if finger_coordinates.keys() >= {"thumb_t", "thumb_k"}:
+        if (calc_landmark_distance(finger_coordinates, "thumb_t", "index_k") > gesture_distances["split_thumb-index"] and
+            calc_landmark_distance(finger_coordinates, "thumb_t", "wrist") > calc_landmark_distance(finger_coordinates, "thumb_k", "wrist")):
             #print("pinch pinky")
             extend_list[0] = True
 
     # Extend index Gesture
-    if fingerCoordinates.keys() >= {"index_t", "index_k"}:
-        if (calc_landmark_distance(fingerCoordinates, "index_t", "index_k") > gesture_distances["extend-index"] and
-            calc_landmark_distance(fingerCoordinates, "index_t", "wrist") > calc_landmark_distance(fingerCoordinates, "index_k", "wrist")):
+    if finger_coordinates.keys() >= {"index_t", "index_k"}:
+        if (calc_landmark_distance(finger_coordinates, "index_t", "index_k") > gesture_distances["extend-index"] and
+            calc_landmark_distance(finger_coordinates, "index_t", "wrist") > calc_landmark_distance(finger_coordinates, "index_k", "wrist")):
             #print("pinch pinky")
             extend_list[1] = True
 
     # Extend middle Gesture
-    if fingerCoordinates.keys() >= {"middle_t", "middle_k"}:
-        if (calc_landmark_distance(fingerCoordinates, "middle_t", "middle_k") > gesture_distances["extend-middle"] and
-            calc_landmark_distance(fingerCoordinates, "middle_t", "wrist") > calc_landmark_distance(fingerCoordinates, "middle_k", "wrist")):
+    if finger_coordinates.keys() >= {"middle_t", "middle_k"}:
+        if (calc_landmark_distance(finger_coordinates, "middle_t", "middle_k") > gesture_distances["extend-middle"] and
+            calc_landmark_distance(finger_coordinates, "middle_t", "wrist") > calc_landmark_distance(finger_coordinates, "middle_k", "wrist")):
             #print("pinch pinky")
             extend_list[2] = True
 
     # Extend ring Gesture
-    if fingerCoordinates.keys() >= {"ring_t", "ring_k"}:
-        if (calc_landmark_distance(fingerCoordinates, "ring_t", "ring_k") > gesture_distances["extend-ring"] and
-            calc_landmark_distance(fingerCoordinates, "ring_t", "wrist") > calc_landmark_distance(fingerCoordinates, "ring_k", "wrist")):
+    if finger_coordinates.keys() >= {"ring_t", "ring_k"}:
+        if (calc_landmark_distance(finger_coordinates, "ring_t", "ring_k") > gesture_distances["extend-ring"] and
+            calc_landmark_distance(finger_coordinates, "ring_t", "wrist") > calc_landmark_distance(finger_coordinates, "ring_k", "wrist")):
             #print("pinch pinky")
             extend_list[3] = True
 
     # Extend pinky Gesture
-    if fingerCoordinates.keys() >= {"pinky_t", "pinky_k"}:
-        if (calc_landmark_distance(fingerCoordinates, "pinky_t", "pinky_k") > gesture_distances["extend-pinky"] and
-            calc_landmark_distance(fingerCoordinates, "pinky_t", "wrist") > calc_landmark_distance(fingerCoordinates, "pinky_k", "wrist")):
+    if finger_coordinates.keys() >= {"pinky_t", "pinky_k"}:
+        if (calc_landmark_distance(finger_coordinates, "pinky_t", "pinky_k") > gesture_distances["extend-pinky"] and
+            calc_landmark_distance(finger_coordinates, "pinky_t", "wrist") > calc_landmark_distance(finger_coordinates, "pinky_k", "wrist")):
             #print("pinch pinky")
             extend_list[4] = True
     
     # Vulcan Gesture
-    if fingerCoordinates.keys() >= {"thumb_k", "index_k", "thumb_t", "index_t", "middle_t", "ring_t", "pinky_t"}:
-        if (calc_landmark_distance(fingerCoordinates, "thumb_k", "index_k") > gesture_distances["split_thumb-index"] and
-            calc_landmark_distance(fingerCoordinates, "index_t", "middle_t") < gesture_distances["clamp_index-middle"] and
-            calc_landmark_distance(fingerCoordinates, "middle_t", "ring_t") > gesture_distances["clamp_index-middle"] and
-            calc_landmark_distance(fingerCoordinates, "ring_t", "pinky_t") < gesture_distances["clamp_ring-pinky"]):
+    if finger_coordinates.keys() >= {"thumb_k", "index_k", "thumb_t", "index_t", "middle_t", "ring_t", "pinky_t"}:
+        if (calc_landmark_distance(finger_coordinates, "thumb_k", "index_k") > gesture_distances["split_thumb-index"] and
+            calc_landmark_distance(finger_coordinates, "index_t", "middle_t") < gesture_distances["clamp_index-middle"] and
+            calc_landmark_distance(finger_coordinates, "middle_t", "ring_t") > gesture_distances["clamp_index-middle"] and
+            calc_landmark_distance(finger_coordinates, "ring_t", "pinky_t") < gesture_distances["clamp_ring-pinky"]):
             #print("vulcan")
             pass
 
     # OK Gesture
-    if fingerCoordinates.keys() >= {"thumb_k", "index_k", "middle_k", "ring_k", "pinky_k", "thumb_t", "index_t", "middle_t", "ring_t", "pinky_t"}:
-        if (calc_landmark_distance(fingerCoordinates, "thumb_t", "index_t") < gesture_distances["pinch-index"] and
-            calc_landmark_distance(fingerCoordinates, "index_k", "middle_t") > gesture_distances["extend-index"] and
-            calc_landmark_distance(fingerCoordinates, "index_k", "index_t") > gesture_distances["extend-index"] and
-            calc_landmark_distance(fingerCoordinates, "middle_k", "middle_t") > gesture_distances["extend-index"] and
-            calc_landmark_distance(fingerCoordinates, "ring_k", "ring_t") > gesture_distances["extend-index"] and
-            calc_landmark_distance(fingerCoordinates, "pinky_k", "pinky_t") > gesture_distances["extend-index"]):
+    if finger_coordinates.keys() >= {"thumb_k", "index_k", "middle_k", "ring_k", "pinky_k", "thumb_t", "index_t", "middle_t", "ring_t", "pinky_t"}:
+        if (calc_landmark_distance(finger_coordinates, "thumb_t", "index_t") < gesture_distances["pinch-index"] and
+            calc_landmark_distance(finger_coordinates, "index_k", "middle_t") > gesture_distances["extend-index"] and
+            calc_landmark_distance(finger_coordinates, "index_k", "index_t") > gesture_distances["extend-index"] and
+            calc_landmark_distance(finger_coordinates, "middle_k", "middle_t") > gesture_distances["extend-index"] and
+            calc_landmark_distance(finger_coordinates, "ring_k", "ring_t") > gesture_distances["extend-index"] and
+            calc_landmark_distance(finger_coordinates, "pinky_k", "pinky_t") > gesture_distances["extend-index"]):
             #print("ok")
             pass
-        #print(calc_landmark_distance(fingerCoordinates, "thumb_t", "index_k"))
+        #print(calc_landmark_distance(finger_coordinates, "thumb_t", "index_k"))
         #print(pinch_list[0], pinch_list[1], pinch_list[2], pinch_list[3])
         #print(extend_list[0], extend_list[1], extend_list[2], extend_list[3], extend_list[4])
-        #print(calc_landmark_distance(fingerCoordinates, "thumb_t", "index_t") < 30,
-        #    calc_landmark_distance(fingerCoordinates, "index_k", "middle_t") > 100,
-        #    calc_landmark_distance(fingerCoordinates, "index_k", "index_t") > 100,
-        #    calc_landmark_distance(fingerCoordinates, "middle_k", "middle_t") > 100,
-        #    calc_landmark_distance(fingerCoordinates, "ring_k", "ring_t") > 100,
-        #    calc_landmark_distance(fingerCoordinates, "pinky_k", "pinky_t") > 100)
+        #print(calc_landmark_distance(finger_coordinates, "thumb_t", "index_t") < 30,
+        #    calc_landmark_distance(finger_coordinates, "index_k", "middle_t") > 100,
+        #    calc_landmark_distance(finger_coordinates, "index_k", "index_t") > 100,
+        #    calc_landmark_distance(finger_coordinates, "middle_k", "middle_t") > 100,
+        #    calc_landmark_distance(finger_coordinates, "ring_k", "ring_t") > 100,
+        #    calc_landmark_distance(finger_coordinates, "pinky_k", "pinky_t") > 100)
 
     hand_plane = []
-    if fingerCoordinates.keys() >= {"wrist"}:
-        hand_plane.append(fingerCoordinates["wrist"])
+    if finger_coordinates.keys() >= {"wrist"}:
+        hand_plane.append(finger_coordinates["wrist"])
     else:
         hand_plane.append((0, 0, 0))
-    if fingerCoordinates.keys() >= {"index_k"}:
-        hand_plane.append(fingerCoordinates["index_k"])
+    if finger_coordinates.keys() >= {"index_k"}:
+        hand_plane.append(finger_coordinates["index_k"])
     else:
         hand_plane.append((0, 0, 0))
-    if fingerCoordinates.keys() >= {"pinky_k"}:
-        hand_plane.append(fingerCoordinates["pinky_k"])
+    if finger_coordinates.keys() >= {"pinky_k"}:
+        hand_plane.append(finger_coordinates["pinky_k"])
     else:
         hand_plane.append((0, 0, 0))
 
@@ -203,6 +203,41 @@ class Ball():
     
     def draw(self, img: cv2.typing.MatLike):
         cv2.circle(img, (self.x, self.y), self.radius, (255, 0, 0), cv2.FILLED)
+
+    def intersects_paddle(self, paddle_start, paddle_end):
+        # Based on code from https://stackoverflow.com/a/67117213
+        x1, y1 = paddle_start
+        x2, y2 = paddle_end
+        cx, cy, r = self.startx, self.starty, self.radius
+        x_linear = x2 - x1
+        x_constant = x1 - cx
+        y_linear = y2 - y1
+        y_constant = y1 - cy
+        a = x_linear * x_linear + y_linear * y_linear
+        half_b = x_linear * x_constant + y_linear * y_constant
+        c = x_constant * x_constant + y_constant * y_constant - r * r
+        return (
+          half_b * half_b >= a * c and
+          (-half_b <= a or c + half_b + half_b + a <= 0) and
+          (half_b <= 0 or c <= 0)
+        )
+
+    def paddle_bounce(self, paddles):
+        for paddle in paddles:
+            if not intersects_paddle(paddle[0], paddle[1]):
+                continue
+            paddle_vec = map(sub, paddle[0], paddle[1])
+            paddle_vec_mag = math.sqrt(sum(map(partial(pow, exp=2), paddle_vec)))
+            paddle_vec_norm = map(partial(mul, 1/paddle_vec_mag), paddle_vec)
+
+            dot_prod_a = sum(map(mul, self.velocity, paddle_vec_norm))
+            proj_a = map(partial(mul, 1/dot_prod_a), paddle_vec_norm)
+
+            paddle_norm_left = (-paddle_vec_norm[1], paddle_vec_norm[0])
+            dot_prod_b = sum(map(mul, self.velocity, paddle_norm_left))
+            proj_b = map(partial(mul, 1/dot_prod_b), paddle_norm_left)
+            new_velocity = proj_a - proj_b
+            self.velocity = new_velocity
     
     def update(self):
         try:
@@ -236,7 +271,7 @@ def calc_paddle(plane_coords):
 
     paddle_direction = (point_a[0] - hand_center[0], point_a[1] - hand_center[1])
     paddle_direction = map(sub, point_a, hand_center)
-    paddle_direction_magnitude = sqrt(sum(map(partial(math.pow, y=2), paddle_direction)))
+    paddle_direction_magnitude = math.sqrt(sum(map(partial(pow, exp=2), paddle_direction)))
     paddle_direction_normalized = map(partial(mul, 1/paddle_direction_magnitude), paddle_direction)
 
     paddle_size = 1
@@ -267,6 +302,7 @@ while True:
     paddle_coords = [calc_paddle(hand) for hand in plane_coords]
 
     ball.update()
+    ball.paddle_bounce(paddle_coords)
     ball.draw(img)
 
     current_time = time.time()
